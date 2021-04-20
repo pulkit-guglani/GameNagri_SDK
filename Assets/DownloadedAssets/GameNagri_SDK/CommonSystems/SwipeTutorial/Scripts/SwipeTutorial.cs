db14c0ffa9dbb909dc739f01e1b8ff92f93e821e
pulkit <pulkit1011@users.noreply.github.com>
2021-04-20 18:24:09 +0530

    init

---
+++ Assets/DownloadedAssets/GameNagri_SDK/CommonSystems/SwipeTutorial/Scripts/SwipeTutorial.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeTutorial : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle DoNotShowAgainToggle;
+    [SerializeField]
+    private GameObject StartGamePanel;
+
+    private void Start()
+    {
+       
+        if (PlayerPrefs.HasKey("DoNotShowSwipeTutorial") && (PlayerPrefs.GetInt("DoNotShowSwipeTutorial") == 1))
+        {
+            ResumeGame();
+        }
+        else
+        {
+            Invoke("PauseGame", 0.05f);
+        }
+
+    }
+    public void PauseGame()
+    {
+        print("pausing game");
+        Time.timeScale = 0;
+    }
+    private void ResumeGame()
+    {
+        print("resuming game");
+
+        Time.timeScale = 1;
+        Destroy(transform.parent.gameObject);
+    }
+
+    public void ShowStartGamePanel()
+    {
+        StartGamePanel.SetActive(true);
+    }
+
+    public void OnStartGameClick()
+    {
+        int toggle = DoNotShowAgainToggle.isOn ? 1 : 0;
+        print("toggle is : " + toggle);
+        PlayerPrefs.SetInt("DoNotShowSwipeTutorial", toggle);
+        ResumeGame();
+    }
+}
