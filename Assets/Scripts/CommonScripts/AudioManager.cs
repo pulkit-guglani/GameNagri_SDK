cf201b346955fd50b8e79b73f42efc0f7ac3dac3
pulkit <pulkit1011@users.noreply.github.com>
2021-04-21 17:47:55 +0530

    SDK completed, just need to add GameManager and AdManager

---
+++ Assets/Scripts/CommonScripts/AudioManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public AudioMixerGroup mixerGroup;
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.name = s.clip.name;
+
+            s.source.outputAudioMixerGroup = mixerGroup;
+        }
+    }
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("Muted", 0);
+    }
+
+    public void ToggleSound()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+            AudioManager.Instance.Play("SoundButton");
+        }
+    }
+
+    public void Play(string sound)
+    {
+        // Debug.Log("sound: " + sound);
+        if(PlayerPrefs.GetInt("Muted") == 0)
+        {
+            Sound s = Array.Find(sounds, item => item.name == sound);
+            // Debug.Log("Playing : " + s);
+            if(s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found!");
+                return;
+            }
+
+            // s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+            // s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+            // Debug.Log("Play");
+            s.source.Play();
+        }
+    }
+
+    public AudioSource GetAudioSourceOf(string audio)
+    {
+        Sound s = Array.Find(sounds, item => item.name == audio);
+        return s.source;
+    }
+}
---
+++ Assets/Scripts/CommonScripts/GameAnalytics.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class GameAnalytics : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject GetDataAndReplace;
+
+    public static GameAnalytics Instance;
+    private float time = 0;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    private void Start()
+    {
+        time = Time.time;
+        GetCurrentLevelEvent();
+    }
+
+    public void GameOverEvent()
+    {
+        Analytics.CustomEvent("GameOverAt_" + GetDataAndReplace);
+    }
+    public void GameLoseEvent()
+    {
+        Analytics.CustomEvent("GameLoseAt_" + GetDataAndReplace);
+    }
+    public void TaskCompleteEvent()
+    {
+        Analytics.CustomEvent("TaskCount_" + GetDataAndReplace);
+    }
+    public void FoundationSubmissionEvent()
+    {
+        Analytics.CustomEvent("FoundationCount_" + GetDataAndReplace);
+    }
+    private void GetCurrentLevelEvent()
+    {
+       Analytics.CustomEvent("Level_" + SceneManager.GetActiveScene().buildIndex);
+    }
+    public void OnAdClickEvent()
+    {
+        Dictionary<string, object> dict = new Dictionary<string, object>() { { "TaskNumber", GetDataAndReplace } };
+
+        Analytics.CustomEvent("AdCLicked", dict) ;
+
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if(pause == true)
+        {
+            time = Time.time - time;
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Level :", currentScene);
+            data.Add("PlayTime :", time);
+            if (currentScene > 0)
+            {
+                data.Add("TaskCompleted :", GetDataAndReplace);
+            }
+
+            Analytics.CustomEvent("QuitGame :", data);
+        }
+    }
+}
